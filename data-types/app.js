9e2b56302cc53b9c7bf26ecdd89edc124f2ef340
TinovH <9019926@student.zadkine.nl>
2022-09-15 13:31:54 +0200

    opdracht 3.1

---
+++ data-types/app.js
@@ -0,0 +1,28 @@
+
+let myName = "Tino";
+let myAge = 16;
+let diploma = true;
+let passed;
+
+document.write(myName)
+document.write(myAge)
+document.write(diploma)
+document.write(passed)
+
+let num1 = 7;
+let num2 = 14;
+
+let answer = num1+num2;
+document.write(answer)
+
+let answer2 = num1*num2;
+document.write(answer2)
+
+let answer3 = num1-num2;
+document.write(answer3)
+
+let answer4 = num1/num2;
+document.write(answer4)
+
+let answer5 = num1%num2;
+document.write(answer5)
