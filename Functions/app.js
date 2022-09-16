a82b782a11234d9e8ff847073a76543f081ec827
TinovH <9019926@student.zadkine.nl>
2022-09-16 10:18:03 +0200

    opdracht 6.1

--- Functions/app.js
+++ Functions/app.js
@@ -1,4 +1,4 @@
-//oefening 1//
+//oefening 1
 const calculate = function(num1, num2) {
     let answer = num1 + num2
     alert(answer)
@@ -6,7 +6,7 @@ const calculate = function(num1, num2) {
 
     calculate (12,40)
 
-//oefening 2//
+//oefening 2
 const multiplyByFive = function(num){
     let answer = num * 5;
     alert(answer);
@@ -14,7 +14,7 @@ const multiplyByFive = function(num){
 
     multiplyByFive(10)
 
-//oefening 3//
+//oefening 3
 const checkInput = function(num1, num2){
     if (num1 > num2) {
         console.log(num1);
@@ -25,7 +25,7 @@ const checkInput = function(num1, num2){
 
     checkInput(10, 7)
 
-//oefening 4//
+//oefening 4
 const calculateMinutes = function(num){
     let answer = num / 60
     console.log(answer);
