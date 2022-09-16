ebcf7dd7bc257a3809b44d45b88d4760e61d2a10
TinovH <9019926@student.zadkine.nl>
2022-09-16 09:34:17 +0200

    opdracht 3.2

---
+++ onderzoek/app.js
@@ -0,0 +1,20 @@
+
+let num = 15;
+
+if( num > 10  ){
+    console.log("ik ben groter");
+} else if ( num < 10 ){
+    console.log("ik ben kleiner");
+} else if ( num = 10){
+    console.log("ik ben gelijk");
+}
+
+
+let num2 = 20;
+
+if ( num == 15 && num2 > 15 ){
+    console.log("num is lager gelijk aan 15 num2 is groter dan 15")
+}
+if (num ==14 || num2 > 15){
+    console.log("een van de 2 is waar")
+}
