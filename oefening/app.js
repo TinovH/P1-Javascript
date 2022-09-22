45fb5ebf8d53aabbbea96694f78c14337874712c
TinovH <9019926@student.zadkine.nl>
2022-09-22 13:59:47 +0200

    oefening

---
+++ oefening/app.js
@@ -0,0 +1,23 @@
+
+
+//oude methode
+function kleur_aanpassen() {
+
+let title = "title";
+let element = document.getElementById(title);
+console.log(element)
+element.style.color = "red";
+element.style.font = "normal";
+element.innerText = "van Schaik is beter dan Berkhout";
+}
+
+//nieuwe methode
+//const kleur_aanpassen = function() {
+//let title = "title";
+//let element = document.getElementById(title);
+//console.log(element)
+//element.style.color = "red";
+//element.style.font = "normal";
+//element.innerText = "van Schaik is beter dan Berkhout";
+//}
+
