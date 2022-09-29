322a28282a2d09eeb92fc5788c21768d6fabd408
TinovH <9019926@student.zadkine.nl>
2022-09-29 13:15:06 +0200

    Opdracht 6.1

---
+++ opdracht 6.1/app.js
@@ -0,0 +1,9 @@
+const kleur_aanpassen = function() {
+let title = "title";
+let element = document.getElementById(title);
+console.log(element)
+element.style.color = "red";
+element.innerHTML = "Inhoud aangepast";
+}
+
+kleur_aanpassen();
