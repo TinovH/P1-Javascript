98cf472eaace8a35d5498ba73c48bafb06fd6d55
TinovH <9019926@student.zadkine.nl>
2022-09-30 09:49:43 +0200

    opdracht 7.2

--- opdracht_7.2/app.js
+++ opdracht_7.2/app.js
@@ -1,12 +1,24 @@
-let element = document.getElementById("paragraaf");
-
+let element = document.getElementById("prod");
 let budget = 100;
-let product = 60;
 
-if (budget > 60) {
-    element.innerHTML = "U heeft genoeg geld!";
-    element.style.color = "green";
-} else if (budget < 60) {
-    element.innerHTML = "Helaas, te weinig geldt";
-    element.style.color = "red";
-} 
+function myFunction() {
+    let product = prompt("Hoe duur is het product", "");
+    if (budget > product) {
+        element.innerHTML = "U heeft genoeg geld!";
+        element.style.color = "green";
+    } else if (budget < product) {
+        element.innerHTML = "Helaas, te weinig geldt";
+        element.style.color = "red";
+    }
+}
+
+
+
+
+//if (budget > 60) {
+//    element.innerHTML = "U heeft genoeg geld!";
+//    element.style.color = "green";
+//} else if (budget < 60) {
+//    element.innerHTML = "Helaas, te weinig geldt";
+//    element.style.color = "red";
+//}
