e3dd11cca01dfc7307767862b9fa9215b20a66d4
TinovH <9019926@student.zadkine.nl>
2022-10-07 12:00:49 +0200

    countdown

--- opdracht_countdown/app.js
+++ opdracht_countdown/app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date("april 24, 2023 12:00:00").getTime();
+    const countDate = new Date("december 26, 2022 11:00:00").getTime();
     const now = new Date().getTime();
     const gap = countDate - now
 
