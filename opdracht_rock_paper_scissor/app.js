dfed89c61461f06cab1b46bf5afb061f5177a222
TinovH <9019926@student.zadkine.nl>
2022-10-07 14:14:28 +0200

    RPS

--- opdracht_rock_paper_scissor/app.js
+++ opdracht_rock_paper_scissor/app.js
@@ -14,61 +14,82 @@ possibleChoices.forEach(button => button.addEventListener('click', (e) => {
     playerChoice.innerHTML = player;
     
     generateComputerChoice();
+    getResult();
 }))
 
 const generateComputerChoice = function() {
     let randomNumber = Math.floor(Math.random() * 3) + 1;
     
-    if (randomNumber = 1) {
-        computer = "rock";
-    } else if (randomNumber = 2) {
-        computer = "paper";
-    } else if (randomNumber = 3) {
-        computer = "scissors";
+    if (randomNumber == 1) {
+        computer = 'rock';
+    } else if (randomNumber == 2) {
+        computer = 'paper';
+    } else if (randomNumber == 3) {
+        computer = 'scissors';
     } 
+    
     computerChoice.innerHTML = computer;
-    console.log(randomNumber)
+    
 }
 
-const getResult = function() {
-    if (computerChoice = playerChoice){
-        result = "gelijkspel"
-    } else if (computer = rock)
+const getResult = function() { 
+    if(player === computer){
+        result = 'gelijkspel!'
+    }
+    else if(player == 'rock'){
+        if(computer == 'paper'){
+            result = 'je hebt verloren!';
+        }else{
+            result = 'je hebt gewonnen!';
+        }
+    }
+    else if(player == 'scissors'){
+        if(computer == 'rock'){
+            result = 'je hebt verloren!';
+        }else{
+            result = 'je hebt gewonnen!';
+        }
+    }
+    else if(player == 'paper'){
+        if(computer == 'scissors'){
+            result = 'je hebt verloren!';
+        }else{
+            result = 'je hebt gewonnen!';
+        }
+    }
+    resultOutput.innerHTML = result;
+}
     
 
+const random = function(){
+    if(player == computer){
+        result = 'gelijkspel!'
+    } else if(computer == 'rock'){
+        if(player == 'paper'){
+            result = 'Je hebt gewonnen!';
+        }
+    } else if(computer == 'rock'){
+        if(player == 'scissors'){
+            result = 'je hebt verloren!';
+        }
+    } else if(computer == 'paper'){
+        if(player = 'scissors'){
+            result = 'je hebt gewonnen!';
+        }
+    } else if(computer == 'paper'){
+        if(player = 'rock'){
+            result = 'je hebt verloren!';
+        }
+    } else if(computer == 'scissorts'){
+        if(player = 'rock'){
+            result = 'je hebt gewonnen!';
+        }
+    } else if(computer == 'scissor'){
+        if(player = 'paper'){
+            result = 'je hebt verloren!';
+        }
+    }
 
-    if(player === computer){
-        result.textContent = 'gelijkspel'
-    }
-    else if(computer == 'rock'){
-        if(player == 'paper'){
-            result.textContent = 'Je hebt gewonnen';
-        }
-    }
-    else if(computer == 'rock'){
-        if(player == 'scissors'){
-            result.textContent = 'je hebt verloren!';
-        }
-    }
-    else if(computer == 'paper'){
-        if(player == 'scissors'){
-            result.textContent = 'je hebt gewonnen!';
-        }
-    }
-    else if(computer == 'paper'){
-        if(player == 'rock'){
-            result.textContent = 'je hebt verloren!';
-        }
-    }
-    else if(computer == 'scissorts'){
-        if(player == 'rock'){
-            result.textContent = 'je hebt gewonnen!';
-        }
-    }
-    else if(computer == 'scissor'){
-        if(player == 'paper'){
-            result.textContent = 'je hebt verloren!';
-        }
-    }
-    resultOutput.innerHTML = getResult()
+    
 } 
+
