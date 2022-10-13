cd25d5879811651a727d90ec376b99aa4d408c89
TinovH <9019926@student.zadkine.nl>
2022-10-13 14:21:38 +0200

    opdracht 9 begin

--- opdracht_8.2/app.js
+++ opdracht_8.2/app.js
@@ -4,21 +4,37 @@ let in2 = document.getElementById("getal2")
 
 
 const add = function() {
+    if (in1.value > 0 && in2.value > 0) {
     answer = Number(in1.value) + Number(in2.value) ;
     out.innerText = "Het antwoord is: " + answer;
+    } else {
+out.innerText = "het getal is te laag";
+}
 }
 
 const minus = function() {
+    if (in1.value > 0 && in2.value > 0) {
     answer = in1.value - in2.value ;
     out.innerText = "Het antwoord is: " + answer;
+    } else {
+    out.innerText = "het getal is te laag";
+}
 }
 
 const multiply = function() {
+    if (in1.value > 0 && in2.value > 0) {
     answer = in1.value * in2.value ;
     out.innerText = "Het antwoord is: " + answer;
+    } else {
+    out.innerText = "het getal is te laag";
+}
 }
 
 const devide = function() {
+    if (in1.value > 0 && in2.value > 0) {
     answer = in1.value / in2.value ;
     out.innerText = "Het antwoord is: " + answer;
+    } else {
+    out.innerText = "het getal is te laag";
+}
 }
